75039d846afad5fc6fd146c792290e8f36f7234a
Niels Vermeiren <nielsvermeiren6@gmail.com>
2020-02-17 13:44:20 +0100

    disable router logging

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import {RouterModule, Routes} from '@angular/router';
 const routes: Routes = [
   { path: 'quiz', loadChildren: './quiz/quiz.module#QuizModule' },
   { path: 'questions', loadChildren: './question/question.module#QuestionModule'},
+  { path: 'users', loadChildren: './user/user.module#UserModule'},
+  { path: 'login', redirectTo: 'users/login' },
   { path: '', pathMatch: 'full', redirectTo: 'questions' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { enableTracing: true })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
