624e930410ee3f8c976f550a3de3a6573f4100d4
Niels Vermeiren <nielsvermeiren6@gmail.com>
2020-02-23 21:57:54 +0100

    bugfix http services were not working anymore due to import of clienttestingmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,10 @@ import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 import {NavigationComponent} from './navigation/navigation.component';
 import {RouterModule} from "@angular/router";
 import {ReactiveFormsModule} from "@angular/forms";
-import {HTTP_INTERCEPTORS, HttpClient, HttpClientModule} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {SharedModule} from "./shared/shared.module";
 import {AuthenticationService} from "./user/shared/service/authentication.service";
 import {AuthorizationHttpInterceptor} from "./shared/authorization-http-interceptor";
-import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 @NgModule({
   declarations: [
@@ -23,14 +22,11 @@ import {HttpClientTestingModule} from "@angular/common/http/testing";
     NoopAnimationsModule,
     RouterModule,
     ReactiveFormsModule,
-    HttpClientTestingModule,
     HttpClientModule,
     SharedModule
   ],
   bootstrap: [AppComponent],
   providers: [
-    HttpClient,
-    HttpClientModule,
     AuthenticationService,
     { provide: HTTP_INTERCEPTORS, useClass: AuthorizationHttpInterceptor, multi: true }
   ]
