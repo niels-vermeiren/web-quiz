e69ecfd8bdfe4ec86053b57c73773ffb4d6f6079
Niels Vermeiren <nielsvermeiren6@gmail.com>
2020-02-25 12:08:19 +0100

    unit test EnumToArrayPipee

--- src/app/shared/enum-to-array-pipe.spec.ts
+++ src/app/shared/enum-to-array-pipe.spec.ts
@@ -1,7 +1,21 @@
-import { EnumToArrayPipe } from './enum-to-array-pipe';
+import {EnumToArrayPipe} from './enum-to-array-pipe';
+import {QuestionType} from "../question/shared/question-type";
 
 describe('EnumToArrayPipe', () => {
+
+  let pipe: EnumToArrayPipe;
+
+  beforeEach(() => {
+    pipe = new EnumToArrayPipe();
+  });
+
   it('should create an instance', () => {
-    expect(new EnumToArrayPipe()).toBeTruthy();
+    expect(pipe).toBeTruthy();
+  });
+
+  it('pipe transforms enum to an array of its values', () => {
+    let values: string[] = pipe.transform(QuestionType);
+    expect(values).toContain("Normal");
+    expect(values).toContain("Multiple choice");
   });
 });
