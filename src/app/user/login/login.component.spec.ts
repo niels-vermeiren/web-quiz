37442693f15cd594d61d60c2a2e7e27d95299cc8
Niels Vermeiren <nielsvermeiren6@gmail.com>
2020-02-25 11:57:54 +0100

    unit testing LoginComponent and RegisterComponent

--- src/app/user/login/login.component.spec.ts
+++ src/app/user/login/login.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {RouterModule} from "@angular/router";
+import {HttpClientModule} from "@angular/common/http";
+import {APP_BASE_HREF} from "@angular/common";
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,7 +12,11 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ],
+      imports: [ ReactiveFormsModule, RouterModule.forRoot([]), HttpClientModule ],
+      providers: [{
+        provide: APP_BASE_HREF, useValue : '/'
+      }]
     })
     .compileComponents();
   }));
@@ -21,5 +29,19 @@ describe('LoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('form should be invalid when fields contain errors', () => {
+    expect(component.loginForm.valid).toBeFalsy();
+    expect(component.email.errors.required).toBeTruthy();
+    expect(component.password.errors.required).toBeTruthy();
+    component.email.setValue("TestUser");
+    expect(component.email.errors.email).toBeTruthy();
+  });
+
+  it('form should be valid when fields contain no errors', () => {
+    component.email.setValue("test@test.be");
+    component.password.setValue("testPassword");
+    expect(component.loginForm.valid).toBeTruthy();
   });
 });
--- src/app/user/register/register.component.spec.ts
+++ src/app/user/register/register.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegisterComponent } from './register.component';
+import {LoginComponent} from "../login/login.component";
+import {ReactiveFormsModule} from "@angular/forms";
+import {RouterModule} from "@angular/router";
+import {HttpClientModule} from "@angular/common/http";
+import {APP_BASE_HREF} from "@angular/common";
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
@@ -8,9 +13,13 @@ describe('RegisterComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RegisterComponent ]
+      declarations: [ RegisterComponent ],
+      imports: [ ReactiveFormsModule, RouterModule.forRoot([]), HttpClientModule ],
+      providers: [{
+        provide: APP_BASE_HREF, useValue : '/'
+      }]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -21,5 +30,19 @@ describe('RegisterComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('form should be invalid when fields contain errors', () => {
+    expect(component.registerForm.valid).toBeFalsy();
+    expect(component.email.errors.required).toBeTruthy();
+    expect(component.password.errors.required).toBeTruthy();
+    component.email.setValue("TestUser");
+    expect(component.email.errors.email).toBeTruthy();
+  });
+
+  it('form should be valid when fields contain no errors', () => {
+    component.email.setValue("test@test.be");
+    component.password.setValue("testPassword");
+    expect(component.registerForm.valid).toBeTruthy();
   });
 });
